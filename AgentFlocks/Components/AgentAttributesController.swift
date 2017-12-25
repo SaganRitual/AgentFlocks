acf80045723bbbd7e9da20cb95378df621c887b5
Rob Bishop <GreatBigBore@gmail.com>
2017-12-25 02:08:08 -0800

    Show all five attributes in the attributes view

--- AgentFlocks/Components/AgentAttributesController.swift
+++ AgentFlocks/Components/AgentAttributesController.swift
@@ -15,43 +15,48 @@ protocol AgentAttributesDelegate {
 
 class AgentAttributesController: NSViewController {
 	
-	enum Attribute {
-		case Speed
-		case Mass
-	}
+	enum Attribute { case mass, maxAcceleration, maxSpeed, radius, scale }
 	
 	// MARK: - Attributes (public)
 	
-	var speed:Double {
+    var mass:Double {
+        get {
+            return massSliderController.value
+        }
+        set {
+            massSliderController.value = newValue
+        }
+    }
+    var maxAcceleration:Double {
+        get {
+            return maxAccelerationSliderController.value
+        }
+        set {
+            maxAccelerationSliderController.value = newValue
+        }
+    }
+    var maxSpeed:Double {
+        get {
+            return maxSpeedSliderController.value
+        }
+        set {
+            maxSpeedSliderController.value = newValue
+        }
+    }
+    var radius:Double {
+        get {
+            return radiusSliderController.value
+        }
+        set {
+            radiusSliderController.value = newValue
+        }
+    }
+	var scale:Double {
 		get {
-			return speedSliderController.value
+			return scaleSliderController.value
 		}
 		set {
-			speedSliderController.value = newValue
-		}
-	}
-	var maxSpeed:Double {
-		get {
-			return speedSliderController.maxValue
-		}
-		set {
-			speedSliderController.maxValue = newValue
-		}
-	}
-	var mass:Double {
-		get {
-			return massSliderController.value
-		}
-		set {
-			massSliderController.value = newValue
-		}
-	}
-	var maxMass:Double {
-		get {
-			return massSliderController.maxValue
-		}
-		set {
-			massSliderController.maxValue = newValue
+			scaleSliderController.value = newValue
 		}
 	}
 
@@ -59,12 +64,18 @@ class AgentAttributesController: NSViewController {
 	
 	// MARK: - Attributes (private)
 	
-	@IBOutlet private weak var speedSliderContainer: NSView!
 	@IBOutlet private weak var massSliderContainer: NSView!
-	
-	private let speedSliderController = SliderController()
+    @IBOutlet private weak var maxAccelerationSliderContainer: NSView!
+    @IBOutlet private weak var maxSpeedSliderContainer: NSView!
+    @IBOutlet private weak var radiusSliderContainer: NSView!
+    @IBOutlet private weak var scaleSliderContainer: NSView!
+
 	private let massSliderController = SliderController()
-	
+    private let maxAccelerationSliderController = SliderController()
+    private let maxSpeedSliderController = SliderController()
+    private let radiusSliderController = SliderController()
+    private let scaleSliderController = SliderController()
+
 	// MARK: - Initialization
 	
 	init() {
@@ -77,12 +88,19 @@ class AgentAttributesController: NSViewController {
 	
     override func viewDidLoad() {
         super.viewDidLoad()
-		
-		speedSliderController.sliderName = "Speed"
-		massSliderController.sliderName = "Mass"
-		
-        speedSliderController.addToView(speedSliderContainer)
-		massSliderController.addToView(massSliderContainer)
+        
+        let attributeSets = [
+            (massSliderContainer, massSliderController, "Mass"),
+            (maxAccelerationSliderContainer, maxAccelerationSliderController, "Max Accel"),
+            (maxSpeedSliderContainer, maxSpeedSliderController, "Max Speed"),
+            (radiusSliderContainer, radiusSliderController, "Radius"),
+            (scaleSliderContainer, scaleSliderController, "Scale")
+        ]
+        
+        for s in attributeSets {
+            s.1.sliderName = s.2
+            s.1.addToView(s.0)
+        }
     }
     
 }
@@ -93,10 +111,16 @@ extension AgentAttributesController: SliderDelegate {
 	
 	func slider(_ controller: SliderController, newValue value: Double) {
 		switch controller {
-		case speedSliderController:
-			delegate?.agent(self, newValue: value, ofAttribute: .Speed)
-		case massSliderController:
-			delegate?.agent(self, newValue: value, ofAttribute: .Mass)
+        case massSliderController:
+            delegate?.agent(self, newValue: value, ofAttribute: .mass)
+        case maxAccelerationSliderController:
+            delegate?.agent(self, newValue: value, ofAttribute: .maxAcceleration)
+        case maxSpeedSliderController:
+            delegate?.agent(self, newValue: value, ofAttribute: .maxSpeed)
+        case radiusSliderController:
+            delegate?.agent(self, newValue: value, ofAttribute: .radius)
+		case scaleSliderController:
+			delegate?.agent(self, newValue: value, ofAttribute: .scale)
 		default:
 			return
 		}
@@ -104,10 +128,16 @@ extension AgentAttributesController: SliderDelegate {
 	
 	func slider(_ controller: SliderController, newMaxValue maxValue: Double) {
 		switch controller {
-		case speedSliderController:
-			delegate?.agent(self, newMaxValue: maxValue, ofAttribute: .Speed)
 		case massSliderController:
-			delegate?.agent(self, newMaxValue: maxValue, ofAttribute: .Mass)
+			delegate?.agent(self, newMaxValue: maxValue, ofAttribute: .mass)
+        case maxAccelerationSliderController:
+            delegate?.agent(self, newMaxValue: maxValue, ofAttribute: .maxAcceleration)
+        case maxSpeedSliderController:
+            delegate?.agent(self, newMaxValue: maxValue, ofAttribute: .maxSpeed)
+        case radiusSliderController:
+            delegate?.agent(self, newMaxValue: maxValue, ofAttribute: .radius)
+        case scaleSliderController:
+            delegate?.agent(self, newMaxValue: maxValue, ofAttribute: .scale)
 		default:
 			return
 		}
